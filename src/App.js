e262f41664c86a25843840660d30948f33c3787e
Cesar Zulueta <cesarzulueta12@gmail.com>
2021-09-10 14:15:50 -0700

    built basic backend server with components and models and routes, along with frontend client folder with react app installed

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Inputs from "./todoInputs.js"
+
+function App() {
+  return (
+    <div className="App">
+      <Inputs />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/todoInputs.js
@@ -0,0 +1,17 @@
+import React, {useState} from "react"
+
+function Inputs() {
+    return(
+        <div>
+            <input
+                className={}
+                name={}
+                value={}
+                placeholder={}
+                onChange={}
+            >
+            </input>
+        </div>
+    )
+}
+export default Inputs
